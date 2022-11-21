d110f8dda05022a317bf1d3bc7054ad8a356a9d2
thehamzaihsan <85873694+thehamzaihsan@users.noreply.github.com>
2022-11-21 17:33:29 +0500

    project commit

---
+++ php/cars.php
@@ -0,0 +1,214 @@
+
+<?php include'header2.php';?>
+
+<div class="bg-img2">
+<div class=" col-sm-12 overlay" align="center">
+<p>Compare your car rental online</p>
+
+<!--1st row-->
+<!--1st row-->
+<div class="row">
+<form>
+<style>
+.ui-datepicker.ui-datepicker-multi{z-index: 99999!important;}
+.ui-state-default, .ui-widget-content .ui-state-default, .ui-widget-header .ui-state-default {padding: 0px 8px;}
+</style>
+<div class="container">
+
+<div id="bookingengine"></div>
+<script>
+var default_values = {
+affiliate_id:"2479",    // your affiliate ID
+page: "step1",
+terms_page:"https://www.foxairfare.com/terms-conditions.php",  // your terms and conditions page
+step2Url: "https://www.foxairfare.com/car-search.php",    //  your search page URL
+formType: "form5",
+custom_css: "https://www.foxairfare.com/css/car-rental-form.css",  // your custom CSS URL
+privacy_page:"https://www.foxairfare.com/cookie.php", // your privacy policy page
+unsubscribe_page:"",   // make it empty as of now (will provide you)
+language: "en", 
+show_multilingual: "0", 
+width: "100%",
+height: "256px",
+pickup_country:"",
+pickup_city:"",
+pickup_loc:"",
+};
+</script>
+<script type="text/javascript" src="//api.supplycars.com/jsbookingengine/script3.js"></script>  
+</div>
+</div>
+<!--<a href="#" class="btnb">Select My Car</a> -->
+</form>
+</div>
+<!--row-->
+<!--1st row-->
+<!---row-->
+</div></div>
+<div class="" align="center" style=" font-size:16px; background: rgba(54, 25, 25, .5); "><div class="res-cnctPrt ">		 <div >					
+		<a rel="nofollow" href="#">	<p>	
+Compare 800 companies at over 49,000 locations and 175 countries		
+	                      </p>			</a>		</div>		</div>	<br>
+
+</div>
+<!--</div>
+</div> -->
+<!---row-->
+
+<!-- </div>
+
+</div> -->
+
+<html>
+
+<body style="background-color:#f2f1f1; overflow-x:hidden;">
+
+<div class="container">
+
+<div class="row">
+
+	<div class="col-sm-12" style="margin-top:-25px; margin-bottom:-20px;" align="center">
+
+		<p class="lil-brake"><h2>Best deals on cheap car hire</h2></p>
+
+        <p>Offer rental cars of guaranteed quality at reasonable tariffs.</p>
+
+	</div>
+
+</div>
+
+<!--row-->
+
+<div class="row">
+
+<div class="row img-row">
+
+	<div class="col-sm-8 hertzss" align="center"   style="">
+
+    	<img src="img/hertz.jpg" class="img-responsive imgs" />
+
+        <div class="overlay-text white-heading">
+
+        
+
+        <p></p>
+
+        </div>
+
+      <a rel="nofollow" href="http://www.tkqlhce.com/click-8131542-11140165?sid=foxairfare" target="_blank" class="btna">Save up to 30%</a>
+
+    </div>
+
+
+
+<div class="col-sm-4 rentAcar">
+
+    	<img src="img/rent_a_cars.jpg " class="img-responsive imgs" />
+
+        <div class="overlay-text white-heading">
+
+        $7.99 per day
+
+			<p></p>
+
+	        </div>
+
+            <a rel="nofollow" href="https://www.rentalcars.com/?affiliateCode=hotravltd897" target="_blank" class="btna">Reserve With Now</a>
+
+			</div>
+
+<!-- row -->
+
+<p><img class="img-responsive" src="img/cars_dealer.png" /></p><!--row-->
+
+</div>
+
+<!--container-->
+
+</div></div>
+
+<div class="container-fluid" style="background:url(img/footer-bg.jpg);">
+
+<div  style="color:#FFF; font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif">
+
+	<div class="container">
+
+		<div class="row">
+			<div class="col-lg-2 col-md-2 col-sm-3">
+				<div style="margin-top:50px; margin-bottom:50px;">
+					 <a href="about-us.php" style="text-decoration:none; font-size:20px; color:#FFF">About us</a><br>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-3">
+				<div style="margin-top:50px; margin-bottom:50px;">
+					 <a href="how-to-book.php" style="text-decoration:none; margin-top:30px; font-size:20px; color:#FFF">How to book</a><br>
+                     </div>
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-3">
+				<div style="margin-top:50px; margin-bottom:50px;">
+					 <a href="terms-of-use.php" style="text-decoration:none; margin-top:30px; font-size:20px; color:#FFF">Terms of Use</a><br>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-2 col-sm-3">
+				<div style="margin-top:50px; margin-bottom:50px;">
+					 <a href="contact.php" style="text-decoration:none; margin-top:30px; font-size:20px; color:#FFF">Contact Us</a><br>
+				</div>
+			</div>
+			
+			<div class="col-lg-2 col-md-2 col-sm-3">
+
+				<div style="margin-top:20px; margin-bottom:10px;">
+				<a rel="nofollow" href="https://www.facebook.com/foxairfare/"><i class="fa fa-facebook social-color"  aria-hidden="true"></i></a>
+                <a rel="nofollow" href="https://twitter.com/foxairfare"><i class="fa fa-twitter social-color" aria-hidden="true"></i></a>
+				</div>
+
+			</div>
+
+		</div>
+
+        </div>
+
+        <!-- <center> -->
+
+        <div style="margin-left:30px" class="col-lg-11">
+
+		<hr color="#333333" align="center" thickness="2px"/>
+
+	   	
+
+
+
+	
+
+    <div style="margin-left:60px" class="col-lg-11">
+
+    <p class="pull-left copy">&copy;&nbsp; 2017 Fox Airfare </p>
+
+    
+
+    
+
+</div><!--row-->
+
+</div><!--container-->
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+</body>
+  <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,3566682,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?3566682&101" alt="" border="0"></a></noscript>
+
+</html>
---
+++ php/header2.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html >
+<html>
+<head>
+<link rel="icon" href="img/foxairfare_logo.png">
+<script src="https://use.fontawesome.com/a58c4dea90.js" defer></script>
+<meta name="viewport" content="width=device-width">
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+
+<!--UI-->
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.min.css">
+  <!-- <link rel="stylesheet" href="/resources/demos/style.css"> -->
+<style>
+.btn.btn-primary.dropdown-toggle {color: #555;background-color: #e7e7e7;border: none;padding: 13px;}
+.btn .caret {margin-left: 5px !important;}
+</style>
+<!--UI-->
+<!--links-->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+<!--links-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Cheap Discounted Rental Cars | FoxAirfare</title>
+<meta name="description" content="Search for the best deals on car rentals 24/7 worldwide at affordable rates. Book now cheap rental cars online & save up to 30% with foxairfare.com. " />
+    <meta name="keywords" content="Cars rental, worlds rent a cars, Rent a cars, Cheap cars, Hotels, rent a cars, cars travel" />
+    <meta name="Copyright" content="Find rental cars to foxairfare.com"/>
+	    <meta name="robots" content="index , follow"/>
+	    <meta name="googlebot" content="index , follow"/>
+	    <meta name="Author" content="Fox Airfare"/>
+		
+		
+
+<!-- Begin Inspectlet Embed Code -->
+<script type="text/javascript" id="inspectletjs">
+window.__insp = window.__insp || [];
+__insp.push(['wid', 570621124]);
+(function() {
+function ldinsp(){if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();
+})();
+</script>
+<!-- End Inspectlet Embed Code -->
+
+<style>
+@media screen and (min-width: 1140px) {
+   
+ #.hertzss{ width:790px}
+ .bg-img2{margin-top:95px;}
+
+
+}
+@media screen and (max-width: 960px) {
+   .rentAcar a{
+ width: 80%;
+    text-align: center;
+
+}
+}
+.bg-img2{
+ height: 700px;
+}
+@media only screen and (max-width : 1200px)  {
+.bg-img2{
+ height: 700px;
+}
+}
+@media only screen and (max-width : 990px)  {
+.bg-img2{
+ height: 900px;
+}
+}
+@media only screen and (max-width : 720px)  {
+
+}
+@media only screen and (max-width : 559px)  {
+.bg-img2{
+ height: 1000px;
+}
+}
+@media only screen and (max-width : 450px)  {
+.bg-img2{
+ height: 1100px;
+}
+}
+.bg-img2{margin-top:-95px;}
+body{overflow-x:hidden;}
+  </style>
+</head>
+<body>
+
+<!--Header-->
+
+
+<nav class="navbar navbar-default navbar-fixed-top" style="background-color:#f2f1f1">
+  <div class="container-fluid ">
+<div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        </button>
+      <a href="https://www.foxairfare.com"><img src="img/foxairfare_logo.png" style="margin-bottom:8px; margin-top:5px;" class="img-responsive" alt="Fox airfare logo"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar"> 
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="hotels.php">Hotels</a></li>
+       <!-- <li class="active"><div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Hotels<span class="caret"></span></button>
+
+    <ul class="dropdown-menu">
+      <li><a href="hotels.php">Hotels</a></li>
+      <li><a href="agoda.php">Agoda</a></li></ul>
+      </div>
+      </li> -->
+        <li class="active"><div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Cars<span class="caret"></span></button>
+
+		<ul class="dropdown-menu"><li><a href="cars.php">Cars</a></li><li><a href="manage-booking.php">Manage Booking</a></li></ul>
+      </div>
+      </li>
+    </ul>
+      </div>
+      </div>
+</nav>
+
+
+<!--Header-->
